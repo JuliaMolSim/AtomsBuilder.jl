5710cbcc0d7175a75156a667d1fe84e30ceef3d5
JuliaMolSim <christohortner@gmail.com>
2024-06-01 10:14:46 -0700

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "AtomsBuilder.jl" begin
     # Write your tests here.
     @testset "bulk" begin include("test_bulk.jl"); end
+    @testset "utils" begin include("test_utils.jl"); end
 end
 
 
