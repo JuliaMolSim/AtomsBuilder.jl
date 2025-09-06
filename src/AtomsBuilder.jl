04cbfaa0ba8524ae5e568830e9bfcb6d440739e5
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2025-09-06 18:48:23 +0300

    improve random species generation

--- src/AtomsBuilder.jl
+++ src/AtomsBuilder.jl
@@ -1,8 +1,19 @@
 module AtomsBuilder
 
-import AtomsBase
-using StaticArrays: SMatrix, SVector
+using AtomsBase 
 using AtomsSystems
+using LinearAlgebra: norm, I 
+using Reexport
+using StaticArrays: SMatrix, SVector
+using StatsBase: sample, AbstractWeights
+using Unitful
+
+@reexport using StatsBase: AnalyticalWeights, FrequencyWeights, ProbabilityWeights, Weights
+
+export bulk
+export rattle! 
+export randz!
+export random_species!
 
 const Mat3{T} = SMatrix{3, 3, T}
 const Vec3{T} = SVector{3, T}
--- src/utils.jl
+++ src/utils.jl
@@ -1,13 +1,5 @@
 
-using AtomsBase 
-using AtomsBase: Atom, FlexibleSystem
-using Unitful: unit, uconvert, ustrip, Quantity
-using LinearAlgebra: norm 
 
-export rattle!, 
-       set_positions, 
-       set_elements, 
-       randz!
 
 """
 Helper function to convert construct a FlexibleSystem from 
@@ -130,14 +122,15 @@ rattle!(sys::FlexibleSystem, r::AbstractFloat) =
 
 
 """
-`randz!(sys::FlexibleSystem, zlist) -> sys`
+   randz!(sys::FlexibleSystem, zlist) -> sys
 
 Randomly assigns elements to the atoms in the system `sys` according to the
 probabilities given in `zlist`.
 `zlist` is an iterable over pairs of the form `id => p` where `id`
 is an atom id (e.g. atomic number or chemical symbol) and `p` 
 a probability. E.g., 
-```
+
+```julia
 sys = bulk(:Ti, cubic=true) * 3
 sys = randz!(sys, [ :Ti => 0.2, :O => 0.8 ])
 ```
@@ -146,24 +139,52 @@ This function was developed mostly for generating testing
 systems. It may not be suitable for generating random alloys. 
 PRs to improve it are welcome. 
 """
-function randz!(sys::FlexibleSystem, zlist)
-   ptot = sum( zp[2] for zp in zlist)
-   plist = [ zp[2] / ptot for zp in zlist ]
-   sump = [ sum(plist[1:i]) for i in 1:length(plist) ]
-   sump[end] = 1.001
-   zlist = [ zp[1] for zp in zlist ]   
+function randz!(sys::AbstractSystem, zlist)
+   species = [ x[1] for x in zlist ]
+   weights = [ x[2] for x in zlist ]
+   return random_species!(sys, species, Weights(weights))
+end
 
-   for i = 1:length(sys)
-      p = rand() 
-      for j = 1:length(sump)
-         if sump[j] > p
-            sys.particles[i] = _set_element(sys.particles[i], zlist[j])
-            break
-         end
-      end
+"""
+   `random_species!(sys::AbstractSystem, species::AbstractVector, weights)`
+
+   `random_species!(sys::AbstractSystem, species::AbstractVector{ChemicalSpecies}, weights)`
+
+Randomly assigns elements to the atoms in the system `sys` according to the
+probabilities given in `weights`.
+
+# Arguments
+- `sys::AbstractSystem`: the system to modify
+- `species::AbstractVector`: a vector of species (either atomic numbers or chemical symbols)
+- `weights`: a `AbstractWeights` object from `StatsBase` giving the probabilities for each species
+
+# Example
+```julia
+using AtomsBuilder
+
+sys = repeat(bulk(:Ti), 3)
+
+spc = [:Ti, :Al]  # species to sample from
+
+# Create weights for each species
+w = AnalyticalWeights([0.5, 0.5])
+w = FrequencyWeights([1, 3])
+w = ProbabilityWeights([0.2, 0.8])
+
+random_species!( sys, spc, w )
+```
+"""
+function random_species!(sys::AbstractSystem, species::AbstractVector, weights)
+   spc = ChemicalSpecies.(species)
+   return random_species!(sys, spc, weights)
+end
+
+function random_species!(sys::AbstractSystem, species::AbstractVector{ChemicalSpecies}, weights::AbstractWeights)
+   for i in 1:length(sys)
+      z = sample(species, weights)
+      AtomsBase.set_species!(sys, i, z)
    end
-
-   return sys 
+   return sys
 end
 
 
